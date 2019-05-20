1fdb819af1323d7825da90d5b1dea4c3633d116d
Matt Rendina <mrendina@stsci.edu>
2019-05-20 10:33:23 -0400

    Store references in a single YAML file Consolidate and generalize methods

--- harbinger/release_notifier.py
+++ harbinger/release_notifier.py
@@ -43,15 +43,27 @@ def pushd(newDir):
 
 
 class Plugin(ABC):
-    def __init__(self):
+    def __init__(self, reference):
         super().__init__()
+        #self.ref_ver_data = None
+        #self.new_ver_data = None
 
     @abstractmethod
-    def get_version(self):
+    def new_version_available(self):
         pass
 
     @abstractmethod
-    def get_changelog(self):
+    def version_data(self):
+        '''Return updated reference values that reflect what was obtained
+        from the version query. All values returned here will become the
+        updated reference dictionary.'''
+        pass
+
+    @abstractmethod
+    def get_extra(self):
+        '''Obtain any extra information the plugin has decided to provide
+        for inclusion in the notification message. Things like changelog
+        entries, API version specifics, etc, may be returned here.'''
         pass
     
 
@@ -68,19 +80,29 @@ class ReleaseNotifier():
     refdir: The directory holding the reference version value for the
             dependency to be queried.
     '''
-    def __init__(self, depname, params, refdir, gh_username=None, gh_password=None):
+    github = None
+
+    def __init__(self,
+                 depname,
+                 params,
+                 refdir,
+                 notify_repo,
+                 gh_username=None,
+                 gh_password=None):
+
         # Normalize path-like dependency names.
         self.dep_name = depname
-        self.depchecker = None
+        self.plugin_module = None
         self.params = params
-        self.github = None
+        #self.github = None
         self.plugin = None
         self.plugin_extra = None  # Optional extra data to pass to a plugin.
+        self.notify_repo = notify_repo
         self.gh_username = gh_username
         self.gh_password = gh_password
         self.refdir = os.path.abspath(refdir)
-        self.ref_file = self.gen_ref_filename()
-        self.new_ver_data = None
+        self.refs_file = os.path.join(self.refdir, 'references.yml')
+        self.refs = None
         self.md5 = None
         self.ref_md5 = None
         self.issue_title_base = 'Upstream release of dependency: '
@@ -90,80 +112,46 @@ class ReleaseNotifier():
                 'releases.\n'.format(self.dep_name))
         self.dry_run = False
         self.remote_ver = None
-
-    def gen_ref_filename(self):
-        '''Generate reference filename
-
-        Returns
-        -------
-        Dependency-specific version reference identifier.
-        '''
-        norm_depname = self.dep_name.replace('/', '-')
-        return os.path.join(self.refdir, '{}_reference'.format(norm_depname))
+        self.read_refs()
 
     def load_plugin(self):
         if len(self.params.keys()) == 0:
             plugin_name = f'.plugins.relcheck_{depname}'
         else:
             plugin_name = '.plugins.' + self.params['plugin'].strip()
-        print(f'plugin_name = {plugin_name}')
+        print(f'plugin: {plugin_name}')
         try:
-            self.depchecker = importlib.import_module(plugin_name, 'harbinger')
+            self.plugin_module= importlib.import_module(plugin_name, 'harbinger')
         except ImportError as e:
             print(f'Import of plugin {plugin_name} failed.\n\n')
             raise(ImportError)
-        self.plugin = self.depchecker.plugin
-        if 'github' in plugin_name:
-            print('Authenticating with github API...')
-            self.github = github3.login(self.gh_username, self.gh_password)
-            self.plugin_extra = self.github
+        # Change to temporary directory before creating plugin object
+        # to provide a clean location for it to create files, if necessary.
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with pushd(tmpdir):
+                self.plugin = self.plugin_module.plugin(self.refs[self.dep_name])
+                # TODO: Detect special extra steps needed by interrogating
+                #       the plugin itself?
+                if 'github' in plugin_name:
+                    print('Authenticating with github API...')
+                    self.github = github3.login(self.gh_username, self.gh_password)
+                    self.plugin_extra = self.github
 
-    def get_version(self):
-        '''Call the version retrieval method of a plugin.
+    def new_version_available(self):
+        return self.plugin.new_version_available()
 
-        This is the version retrieval entry point for the plugin assigned
-        as the release checker for this notifier instance.
+    def version_data(self):
+        return self.plugin.version_data()
 
-        Returns
-        -------
-        Return value of the get_version method of the plugin associated with
-        the dependency in question which is a dict containing at least a
-        'version' key.
-        '''
-        return self.plugin.get_version(self.dep_name, self.params, self.plugin_extra)
+    def get_extra(self):
+        return self.plugin.get_extra()
 
-    def get_changelog(self, ref_ver_data, new_ver_data):
-        '''Call the changelog retrieval method of a plugin.
+    def get_new_reference(self):
+        return self.plugin.get_new_reference()
 
-        This is the changelog retrieval entry point for the plugin assigned
-        as the release checker for this notifier instance.
-
-        Returns
-        -------
-        Return value of the get_changelog method of the plugin associated with
-        the dependency in question which is a string.
-        '''
-        return self.plugin.get_changelog(ref_ver_data, new_ver_data, self.plugin_extra)
-
-    def new_version(self):
-        '''Determine if the version of this dependency is newer than the value
-        stored in 
-
-        Test whether or not the version data for the dependency in question is
-        newer than the reference value stored.
-
-        Returns
-        -------
-        True if version info retrieved is newer than the reference value.
-        False otherwise.
-        '''
-        print('Reading version reference info from: {}'.format(self.ref_file))
-        with open(self.ref_file) as f:
-            ver_info = yaml.safe_load(f)
-        if self.remote_ver != ver_info['version']:
-            return True
-        else:
-            return False
+    def read_refs(self):
+        with open(self.refs_file) as f:
+            self.refs = yaml.safe_load(f)
 
     def reference_available(self):
         '''Is version reference data available for the dependency?
@@ -173,88 +161,56 @@ class ReleaseNotifier():
         True if reference file is available.
         False otherwise.
         '''
-        if os.path.isfile(self.ref_file):
+        if self.dep_name in self.refs.keys():
             return True
         else:
             return False
 
-    def read_reference(self):
-        '''Read YAML dependency version reference data
-
-        Store the YAML in the self.ref_ver_data dict.
-        '''
-        with open(self.ref_file) as f:
-            self.ref_ver_data = yaml.safe_load(f)
-
-    def create_github_issue(self):
+    def post_github_issue(self):
         # Push changes text to a new/existing issue on Github.
         self.comment = self.comment_base + '/n' + self.comment
         if self.dry_run:
             print(self.comment)
         else:
             print('Posting comment to Github...')
-            if not self.github:
-                gh = github3.login(self.gh_username, password=self.gh_password)
-            repo = args.notify_repo.split('/')
+            #if not self.github:
+            if not ReleaseNotifier.github:
+                print('authenticating')
+                #self.github = github3.login(self.gh_username,
+                #                            password=self.gh_password)
+                ReleaseNotifier.github = github3.login(self.gh_username,
+                                            password=self.gh_password)
+            repo = self.notify_repo.split('/')
             print('posting now')
-            self.github.create_issue(repo[0], repo[1], self.issue_title, self.comment)
+            ReleaseNotifier.github.create_issue(repo[0],
+                    repo[1],
+                    self.issue_title,
+                    self.comment)
 
-    def write_version_ref(self):
-        with open(self.ref_file, 'w') as f:
-            #reference = yaml.safe_dump(self.new_ver_data)
-            #print(reference)
-            #f.write(reference)
-            #print(f'WRITE VERSION REF {reference}')
-            f.write(str(self.new_ver_data))
+    def write_refs(self):
+        with open(self.refs_file, 'w') as f:
+            f.write(yaml.safe_dump(self.refs))
 
-    def update_version_ref(self):
-        # Update version reference to inform the next run.
-        #os.chdir(sys.path[0])
-        print('Backing up old version reference...')
-        self.ref_backup = self.ref_file + '.bkup'
-        shutil.copy(self.ref_file, self.ref_backup)
-        print('Updating {} version refrence.  '.format(self.dep_name), end='')
-        try:
-            self.write_version_ref()
-            print('Done.')
-        except e:
-            print('\nERROR writing reference file. To provide the correct '
-                  'reference\nfor the next run and avoid duplicated Github '
-                  'issue comments, \ncopy the following line as the sole '
-                  'contents the file \n{}.\n\n'
-                  '{}'.format(self.ref_file, reference))
-
-    def post_notification(self):
-        self.update_version_ref()
-        try:
-            self.create_github_issue()
-        except e:
-            # roll back version reference
-            shutilcopy(self.ref_backup, self.ref_file)
+    def update_ref_data(self):
+        '''Update version reference to inform the next run.'''
+        self.refs[self.dep_name] = self.version_data()
 
     def check_for_release(self):
         self.load_plugin()
-        with tempfile.TemporaryDirectory() as self.tmpdir:
-            with pushd(self.tmpdir):
-                self.new_ver_data = self.get_version()
-                self.remote_ver = self.new_ver_data['version']
-            # Check for presence of version reference. If it does not exist,
-            # bootstrap future queries by storing the current remote version
-            # info as the reference.
-            if self.reference_available():
-                self.read_reference()
-                if self.new_version():
-                    with pushd(self.tmpdir):
-                        self.comment = self.get_changelog(
-                                self.ref_ver_data, self.new_ver_data)
-                        self.post_notification()
-                else:
-                    print('No new version detected for {}.'.format(
-                        self.dep_name))
-            else:
+        if self.new_version_available():
+            print(f'A version change has been detected for {self.dep_name}')
+            if not self.reference_available():
                 print('No existing version reference found for {}'.format(
                     self.dep_name))
                 print('Storing remote version as new reference: {}'.format(
-                    os.path.join(self.refdir, self.ref_file)))
-                self.write_version_ref()
+                    os.path.join(self.refdir, self.refs_file)))
+            print(f'Reference: {self.refs[self.dep_name]}')
+            self.comment = self.get_extra()
+            self.update_ref_data()
+            print(f'New      : {self.refs[self.dep_name]}')
+            self.write_refs()
+            # TODO: Implement version roll-back if issue posting fails.
+            self.post_github_issue()
+        else:
+            print(f'No new version detected for {self.dep_name}')
 
