5e00ef452b718c590f20f2ad631a780929f2442d
Matt Rendina <mrendina@stsci.edu>
2019-05-20 10:32:54 -0400

    Streamline cfitsio plugin code. tested

--- harbinger/plugins/relcheck_cfitsio.py
+++ harbinger/plugins/relcheck_cfitsio.py
@@ -1,20 +1,23 @@
 # cfitsio-specific version update checker
-# Two functions
-# get_version()
-# get_changelog()
 
 import urllib.request
 import tarfile
+import copy
 
 from ..release_notifier import Plugin
 
 class plugin(Plugin):
 
-    def get_version(dep_id, config, extra=None):
-        verinfo = {}
+    def __init__(self, ref_ver_data):
+        '''Download and extract key files from source tarball.
+        Read in header file.
+        Read in changelog file.'''
+
+        self.ref_ver_data = ref_ver_data
+        self.new_ver_data = copy.deepcopy(self.ref_ver_data)
+
         latest_tar = 'cfitsio_latest.tar.gz'
-        latest_URL = 'http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/{}'.format(latest_tar)
-        reference_file = './cfitsio_reference'
+        latest_URL = f'http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/{latest_tar}'
         fitsio_h = 'cfitsio/fitsio.h'
         changesfile = 'cfitsio/docs/changes.txt'
     
@@ -23,53 +26,54 @@ class plugin(Plugin):
         tfile = tarfile.open(latest_tar, mode='r')
         tfile.extract(fitsio_h)
         tfile.extract(changesfile)
-        ef = open(fitsio_h)
-        lines = ef.readlines()
-    
-        # Extract version and SONAME values from the source code.
-        for line in lines:
+        with open(fitsio_h, 'r') as f:
+            self.header = f.readlines()
+        with open(changesfile) as f:
+            self.changelog = f.readlines()
+        # Extract version value from the source code. Update new_ver_data.
+        for line in self.header:
             if 'CFITSIO_VERSION' in line.strip():
-                version = line.strip().split()[2]
-                print('New version       {}'.format(version))
+                self.version = line.strip().split()[2]
+                #print(f'New version       {self.version}')
+                self.new_ver_data['version'] = self.version
                 break
-        for line in lines:
+        # Extract SONAME value from the source code. Update new_ver_data.
+        self.soname = None
+        for line in self.header:
             if 'CFITSIO_SONAME' in line.strip():
-                soname = line.strip().split()[2]
-                print('New SONAME        {}'.format(soname))
+                self.soname = line.strip().split()[2]
+                #print(f'New SONAME        {self.soname}')
+                self.new_ver_data['soname'] = self.soname
                 break
-    
-        verinfo['version'] = version
-        verinfo['soname'] = soname
-    
-        return(verinfo)
-        
-    
-    def get_changelog(ref_ver_data, new_ver_data, extra=None):
-        ''' ******* Dependency-specific
-        Accepts refvals object that gets returned from get_versions()'''
-        fitsio_h = 'cfitsio/fitsio.h'
-        latest_tar = 'cfitsio_latest.tar.gz'
-        changesfile = 'cfitsio/docs/changes.txt'
-        tfile = tarfile.open(latest_tar, mode='r')
-        tfile.extract(changesfile)
-        changelog = ''
-        with open(changesfile) as cf:
-            sec_open = False
-            for line in cf.readlines():
-                if 'Version' in line.strip() and sec_open:
-                    break
-                if 'Version' in line.strip() and not sec_open:
-                    sec_open = True
-                    changelog += line
-                    continue
-                if sec_open:
-                    changelog += line
-                    continue
-        ef = open(fitsio_h)
-        lines = ef.readlines()
-        ref_soname = ref_ver_data['soname']
-        soname = new_ver_data['soname']
-        if ref_soname != soname:
-            changelog += '\n**NOTE: This release introduces a SONAME change from {} to {}.**'.format(
-                ref_soname, soname)
-        return(changelog)
+
+    def new_version_available(self):
+        return self.new_ver_data['version'] != self.ref_ver_data['version']
+
+    def version_data(self):
+        '''Return reference dict with updated version and other values.'''
+        return(self.new_ver_data)
+
+    def get_extra(self):
+        '''Return changelog and SONAME version comparison information for
+        inclusion in the notification message.'''
+        # TODO: Look into a regex solution to this?
+        sec_open = False
+        latest_changes = ''
+        for line in self.changelog:
+            if 'Version' in line.strip() and sec_open:
+                break
+            if 'Version' in line.strip() and not sec_open:
+                sec_open = True
+                latest_changes += line
+                continue
+            if sec_open:
+                latest_changes += line
+                continue
+        ref_soname = self.ref_ver_data['soname']
+        if ref_soname != self.soname:
+            latest_changes += (
+                    f'\n**NOTE: This release introduces a SONAME'
+                    f'change from {ref_soname} to {self.soname}.**'
+            )
+        return(latest_changes)
+
