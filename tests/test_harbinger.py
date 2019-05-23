573a74cfb0f0cda94f6dc9d46ffaa7ddd2f734d8
Matt Rendina <mrendina@stsci.edu>
2019-05-23 12:13:41 -0400

    Base tests

--- tests/test_harbinger.py
+++ tests/test_harbinger.py
@@ -1,37 +1,80 @@
+import os
+import shutil
+import ast
+import yaml
 import pytest
 from harbinger.release_notifier import *
 
-#@pytest.fixture(autouse=True)
-depname = 'rendinam/auto-test'
-params = {'plugin': 'relcheck_github', 'release_style': 'github'}
-refdir = 'references'
-#@pytest.fixture()
-#def notifier():
-#    notifier = ReleaseNotifier(depname, params, refdir)
-#    #:yield notifier
-#    return notifier
+depname = 'test'
+params = {'plugin': 'relcheck_test'}
+refdir = os.path.abspath('tests')
+notify_repo = 'testorg/testrepo'
+test_reference = {'version': '0.0.0'}
+
+# TODO: Conduct all tests in a sandbox.
+# TODO: Write out the reference file into a known location for each test that
+# requires it.
+
+# Fixtures
+@pytest.fixture(scope='module')
+def notifier():
+    n = ReleaseNotifier(depname,
+                        params,
+                        refdir,
+                        notify_repo)
+    return(n)
+
+#-----
+
+def test_load_plugin(notifier):
+    notifier.load_plugin()
 
 
-def test_gen_ref_filename():
-    noti = ReleaseNotifier(depname, params, refdir)
-    assert noti.gen_ref_filename() == 'references/rendinam-auto-test_reference'
+def test_new_version_available(notifier):
+    notifier.load_plugin()
+    assert notifier.new_version_available() == True
 
-#def test_query_remote(notifier):
-#    print(notifier.depchecker)
 
-def test_get_version():
-    noti = ReleaseNotifier(depname, params, refdir)
-    noti.query_remote()
-    assert noti.get_version() == {'version': 'tag_2'}
+def test_version_data(notifier):
+    notifier.load_plugin()
+    assert notifier.version_data() == {'version': '0.0.0'}
 
-def test_get_changelog():
-    noti = ReleaseNotifier(depname, params, refdir)
-    noti.query_remote()
-    assert noti.get_changelog({'version': 'tag_2'}) == {'version': 'tag_3'}
 
-def test_full_check():
-    noti = ReleaseNotifier(depname, params, refdir)
-    noti.check_for_release()
+def test_get_extra(notifier):
+    notifier.load_plugin()
+    assert notifier.get_extra() == 'Extra info'
 
-        
-    
+
+def test_reference_available(notifier):
+    assert notifier.reference_available() == True
+
+
+def test_reference_not_available(notifier):
+    notifier.dep_name = None
+    assert notifier.reference_available() == False
+
+#def test_post_github_issue():
+#    pass
+
+
+def test_read_refs(notifier):
+    notifier.read_refs()
+    assert notifier.refs == {'test': {'version': '0.0.0'}}
+
+
+def test_write_refs(notifier, tmp_path):
+    notifier.refs_file = os.path.join(tmp_path, 'reference.yml.test')
+    notifier.write_refs()
+    # Read in and compare
+    with open(notifier.refs_file, 'r') as f:
+        new_refs = yaml.safe_load(f.read())
+    assert new_refs[depname]['version'] == notifier.refs[depname]['version']
+
+
+#def test_full_check():
+#    noti = ReleaseNotifier(depname, params, refdir)
+#    noti.check_for_release()
+
+
+#def test_harbinger_cli():
+
