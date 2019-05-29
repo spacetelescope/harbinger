881fdc51b0b4e31be3836f01dec8bdbca28bcb38
Matt Rendina <mrendina@stsci.edu>
2019-05-29 17:29:19 -0400

    Segment out scanner class and add new main routine.

---
+++ harbinger/cli/main.py
@@ -0,0 +1,120 @@
+# Iterate over all repos in a given org (or a single repo specified directly)
+# For each repo that has a harbinger config file in the root dir of the
+#   'master' branch, read the config and associate it with the repo name.
+# For each dependency that has not yet been 
+import os
+import sys
+import argparse
+import github3
+import urllib
+import configparser
+import yaml
+import json
+
+from harbinger.release_notifier import *
+from harbinger.scanner import *
+
+def main():
+
+    username_envvar = 'HARBINGER_USER'
+    password_envvar = 'HARBINGER_PW'
+
+    descrip = (f'Scan a Github organization or user account for repositories that'
+    f' contain a harbinger.cfg file defining dependencies to monitor and '
+    f'post a notification via Github issue if a release newer than the latest'
+    f' logged version is available. Best run on a schedule to continuously '
+    f'monitor for dependency updates.\n')
+    parser = argparse.ArgumentParser(prog='harbinger', description=descrip)
+    parser.add_argument('-p',
+                        '--password',
+                        action='store_true',
+                        help=f'Prompt for interactive password entry. Overrides'
+                        ' default behavior of accepting the password to use via '
+                        ' the environment variable {password_envvar}.')
+    parser.add_argument('-d',
+                        '--dry-run',
+                        action='store_true',
+                        help='All output to local console. Does not communicate '
+                        ' with Github.')
+    parser.add_argument('-u',
+                        '--username',
+                        type=str,
+                        help=f'Prompt for interactive username entry. Overrides'
+                        ' default behavior of accepting the username to use via '
+                        ' the environment variable {username_envvar}.')
+    parser.add_argument('-r',
+                        '--refdir',
+                        type=str,
+                        help='Directory where program will check for the reference'
+                        ' version file. When the flag is not used, the default is '
+                        'the current working directory.')
+    parser.add_argument('-o',
+                        '--org',
+                        type=str,
+                        help='Github organization (or user account) scan.')
+    parser.add_argument('-n',
+                        '--notify_repo',
+                        type=str,
+                        help='Required. Repository in which an issue comment '
+                        'should be posted. Specified in the form '
+                        '<organization>/<repository>')
+   
+    args = parser.parse_args()
+    
+
+    if not args.dry_run:
+        if not args.username:
+            try:
+                username = os.environ[username_envvar]
+            except KeyError:
+                print('Environment variable {} not defined.')
+                print('Store the Github password in that variable or run with '
+                      '`-p` to prompt for the password interactively.'.format(
+                          username_envvar))
+                sys.exit(1)
+        else:
+            username = args.username
+    
+        if args.password:
+            password = getpass.getpass()
+        else:
+            try:
+                password = os.environ[password_envvar]
+            except KeyError:
+                print('Environment variable {} not defined.')
+                print('Store the Github password in that variable or run with '
+                      '`-p` to prompt for the password interactively.'.format(
+                          password_envvar))
+                sys.exit(1)
+    
+    if not args.refdir:
+        refdir = './'
+    else:
+        refdir = args.refdir
+
+    org = args.org
+    
+    username = os.environ[username_envvar]
+    password = os.environ[password_envvar]
+    
+    
+    scanner = Scanner(org, 'references', username, password)
+    repos = scanner.get_repos()
+    scanner.scan()
+    #print(scanner.dep_requests)
+
+    scanner.check_for_releases()
+    scanner.write_refs()
+
+    #print('\nDependencies processed')
+    #for repo in scanner.dep_requests:
+    #    print(repo)
+    #    print(scanner.dep_requests[repo].keys())
+    #    for dep in scanner.notifiers.keys():
+    #        if dep in scanner.dep_requests[repo].keys():
+    #            print(f'Notification sent for {dep}')
+    #    
+    #for dep in scanner.notifiers.keys():
+    #    print(dep)
+    
+    
--- harbinger/scanner.py
+++ harbinger/scanner.py
@@ -1,90 +1,66 @@
-# Iterate over all repos in a given org (or a single repo specified directly)
-# For each repo that has a harbinger config file in the root dir of the
-#   'master' branch, read the config and associate it with the repo name.
-# For each dependency that has not yet been 
-
-import github3
+import os
 import urllib
 import configparser
 import yaml
-
-from harbinger.release_notifier import *
-
-user = True
-
-orgs = ['spacetelescope']
-# For orgs
-#gh = github3.GitHub()
-#reponames = []
-#for org in orgs:
-#    org = gh.organization(org)
-#    repos = list(org.repositories())
-#    for repo in repos:
-#        reponames.append(repo.name)
-#print(reponames)
-
-dep_requests = {}
-cfg_file = 'harbinger.cfg'
-if user:
-    user = 'rendinam'
-    #repos = ['harbinger', 'test-1', 'test-2']
-    repos = ['test-1', 'test-2']
-    #repos = ['test-2']
-    for repo in repos:
-        url = f'https://raw.githubusercontent.com/{user}/{repo}/master/{cfg_file}'
-        request = urllib.request.Request(url)
-        result = urllib.request.urlopen(request)
-        rawconfig = str(result.read().decode())
-        config = configparser.ConfigParser()
-        config.read_string(rawconfig)
-        sections = config.sections()
-        repoconfig = {}
-        for section in sections:
-            repoconfig[section] = dict(config[section])
-        dep_requests[repo] = repoconfig
-
-username_envvar = 'HARBINGER_USER'
-password_envvar = 'HARBINGER_PW'
-username = os.environ[username_envvar]
-password = os.environ[password_envvar]
-
-#for repo in dep_requests:
-#    for dep in dep_requests[repo]:
-#        print(repo, dep, dep_requests[repo][dep])
-#        noti = ReleaseNotifier(dep,
-#                               dep_requests[repo][dep],
-#                               '../references',
-#                               'rendinam/auto-test',
-#                               username,
-#                               password)
-#        noti.check_for_release()
-
+import github3
+import json
+from .release_notifier import *
 
 class Scanner():
-    def __init__(self, refdir, repos):
+
+    def __init__(self, org, refdir, username=None, password=None):
         self.refs = None
-        self.repos = repos
+        self.org = org
         self.refdir = os.path.abspath(refdir)
         self.refs_file = os.path.join(self.refdir, 'references.yml')
         self.read_refs()
         self.dep_requests = {}
         self.processed = []
         self.notifiers = {}
-        self.scan()
+        self.cfg_file = 'harbinger.cfg'
+        self.username = username
+        self.password = password
+        self.gh = github3.GitHub(username, password)
+        self.acc = self.gh.user(org)
+
+    def getjson(self, url):
+        request = urllib.request.Request(url)
+        result = urllib.request.urlopen(request)
+        payload = result.read()
+        jdata = json.loads(payload.decode())
+        return jdata
+
+    def get_repos(self):
+        repos = []
+        page = 1
+        jdata = [1]
+        while jdata != []:
+            url = f'{self.acc.repos_url}?per_page=100&page={page}'
+            jdata = self.getjson(url)
+            for repo in jdata:
+                repos.append(repo['name'])
+            page += 1
+        self.repos = repos
+        return repos
 
     def read_refs(self):
         with open(self.refs_file) as f:
             self.refs = yaml.safe_load(f)
 
     def scan(self):
+        print(f'Scanning {self.org}...')
         for repo in self.repos:
-            print(f'Scanning {repo}...')
             # NOTE: This raw link takes a nonzero amount of time to reflect
             #       the file contents. Try using github3 instead to get a raw
             #       file blob.
-            url = f'https://raw.githubusercontent.com/{user}/{repo}/master/{cfg_file}'
+            base_url = 'https://raw.githubusercontent.com/'
+            url = f'{base_url}{self.org}/{repo}/master/{self.cfg_file}'
             request = urllib.request.Request(url)
-            result = urllib.request.urlopen(request)
+            try:
+                result = urllib.request.urlopen(request)
+            except urllib.error.HTTPError as e:
+                continue
+            print(f'{repo}: Found config')
             rawconfig = str(result.read().decode())
             config = configparser.ConfigParser()
             config.read_string(rawconfig)
@@ -95,44 +71,25 @@ class Scanner():
             self.dep_requests[repo] = repoconfig
 
     def check_for_releases(self):
+        self.repos = self.get_repos()
         for repo in self.dep_requests:
-            print(f'    {repo}')
+            print(f'\nProcessing deps defined in {repo}')
             for dep in self.dep_requests[repo]:
-                    print(f'              {dep}')
-
-        for repo in self.dep_requests:
-            print(f'    {repo}')
-            for dep in self.dep_requests[repo]:
-                print(f'              {dep}')
+                print(f'   {dep}')
                 if dep not in self.notifiers:
                     ref = self.refs[dep]
                     noti = ReleaseNotifier(dep,
-                                           dep_requests[repo][dep],
+                                           self.dep_requests[repo][dep],
                                            ref,
-                                           f'{user}/{repo}',
-                                           username,
-                                           password)
+                                           f'{self.org}/{repo}',
+                                           self.gh)
                     noti.check_for_release()
                     self.notifiers[dep] = noti
                     if noti.new_version_detected:
-                        print('NEW VERSION')
                         self.refs[dep] = noti.ref
                 if self.notifiers[dep].new_version_detected:
-                    self.notifiers[dep].post_github_issue(f'{user}/{repo}')
-
+                    self.notifiers[dep].post_github_issue(f'{self.org}/{repo}')
                 
     def write_refs(self):
         with open(self.refs_file, 'w') as f:
             f.write(yaml.safe_dump(self.refs))
-
-##### main
-
-scanner = Scanner('references', repos)
-scanner.check_for_releases()
-scanner.write_refs()
-print('\nDependencies processed')
-for dep in scanner.notifiers.keys():
-    print(dep)
-
-
-
